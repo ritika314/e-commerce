46a6d2278cd8c0640162d8092dfaf57bc81e79d8
ritika314 <ritikadeogharia87@gmail.com>
2026-06-18 17:56:07 +0530

    header updated

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,11 +1,33 @@
-"use client"
-import React from 'react'
-import "./Header.css"
+"use client";
+import React from "react";
+import "./Header.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { FaRegUser } from "react-icons/fa";
+import { TiShoppingCart } from "react-icons/ti";
+ 
+import "./Header.css";
 
-const Header : React.FC = () => {
+const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>  
+      <div className="main_header">
+        <div className="header_left">
+          <div className="header_left_icon">
+            <AiOutlineHome />
+          </div>
+          <div className="header_title">Shop Ease</div>
+        </div>
+        <div className="header_right">
+          <div className="header_right_icon">
+            <FaRegUser />
+          </div>
+          <div className="header_right_icon">
+            <TiShoppingCart />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
