c4cd7f49466c380dffb709110c4b0eb538581fd2
ritika314 <ritikadeogharia87@gmail.com>
2026-06-26 17:16:32 +0530

    updated category

--- components/homeComponents/HomeCategory.tsx
+++ components/homeComponents/HomeCategory.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import './HomeCategory.css'
 import { categories } from '@/data/productsData'
+import Image from 'next/image'
 const HomeCategory: React.FC = () => {
   return (
     <div className='home_category_section'>
@@ -9,10 +10,23 @@ const HomeCategory: React.FC = () => {
         <div className="category_images">
             {categories.map((category ,index) => {
                 return(
-                    <div key={index} className='cat_card'style={{backgroundImage:`url(${category.image})`}}>
-                        <div className='cat_name'>{category.name}</div>
-                        
-                    </div>)
+                    <div key={index} className='cat_card'>
+                        <Image
+
+                        src={category.image}
+                        alt={category.name}
+                        className="category card img"
+                        width={100}
+                        height={100}
+                        unoptimized
+                        style={{
+                            width: "100% !important",
+                            height: "100% !important",
+                        }}
+                    />
+                    <div className='cat_name'>{category.name}</div>
+                    </div>
+                )
 })
             }
         </div>
