01e58060329654151cede766a14c373e665acc07
ritika314 <ritikadeogharia87@gmail.com>
2026-07-10 18:17:07 +0530

    created checkout

---
+++ app/checkout/page.tsx
@@ -0,0 +1,17 @@
+import CheckoutSection from '@/components/checkoutComponents/CheckoutSection'
+import Footer from '@/components/layouts/Footer'
+import Header from '@/components/layouts/Header'
+import React from 'react'
+
+const page: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <CheckoutSection />
+      <Footer />
+    </>
+  )
+}
+
+export default page
+
---
+++ interfaces/checkout.ts
@@ -0,0 +1,6 @@
+export interface ICheckoutForm {
+  name: string;
+  email: string;
+  mobile: string;
+  address: string;
+}
